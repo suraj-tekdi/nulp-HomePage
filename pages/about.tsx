0f7c910b965ea3e9846dadcfb046aef88ede5b65
pmulik34 <poojamulik34@gmail.com>
2025-09-10 10:45:18 +0530

    added orderings to about us page

--- pages/about.tsx
+++ pages/about.tsx
@@ -1,21 +1,55 @@
-import React from 'react';
-import Head from 'next/head';
-import { Header, Footer } from '../components';
-import AboutUsHero from '../components/AboutUsHero/AboutUsHero';
-import AboutUsVision from '../components/AboutUsVision/AboutUsVision';
-import AboutUsMission from '../components/AboutUsMission/AboutUsMission';
-import AboutUsApproach from '../components/AboutUsApproach/AboutUsApproach';
-import AboutUsOfferings from '../components/AboutUsOfferings/AboutUsOfferings';
-import AboutUsWhy from '../components/AboutUsWhy/AboutUsWhy';
-import AboutUsPartner from '../components/AboutUsPartner/AboutUsPartner';
-import AboutUsContact from '../components/AboutUsContact/AboutUsContact';
+import React from "react";
+import Head from "next/head";
+import type { GetStaticProps } from "next";
+import { Header, Footer } from "../components";
+import AboutUsHero from "../components/AboutUsHero/AboutUsHero";
+import AboutUsVision from "../components/AboutUsVision/AboutUsVision";
+import AboutUsMission from "../components/AboutUsMission/AboutUsMission";
+import AboutUsApproach from "../components/AboutUsApproach/AboutUsApproach";
+import AboutUsOfferings from "../components/AboutUsOfferings/AboutUsOfferings";
+import AboutUsWhy from "../components/AboutUsWhy/AboutUsWhy";
+import AboutUsPartner from "../components/AboutUsPartner/AboutUsPartner";
+import AboutUsContact from "../components/AboutUsContact/AboutUsContact";
+import { articlesApi, type HomepageArticleItem } from "../services";
 
-const AboutPage: React.FC = () => {
+type AboutPageProps = {
+  orderedSectionSlugs?: string[];
+};
+
+const sectionComponents: Record<string, React.FC> = {
+  "our-vision": AboutUsVision,
+  "our-mission": AboutUsMission,
+  "our-approach": AboutUsApproach,
+  "our-offerings": AboutUsOfferings,
+  "why-does-nulp-exist": AboutUsWhy,
+  "partner-with-us": AboutUsPartner,
+};
+
+const DEFAULT_SECTION_ORDER: string[] = [
+  "our-vision",
+  "our-mission",
+  "our-approach",
+  "our-offerings",
+  "why-does-nulp-exist",
+  "partner-with-us",
+];
+
+const AboutPage: React.FC<AboutPageProps> = ({
+  orderedSectionSlugs = DEFAULT_SECTION_ORDER,
+}) => {
+  const sections =
+    Array.isArray(orderedSectionSlugs) && orderedSectionSlugs.length > 0
+      ? orderedSectionSlugs
+      : DEFAULT_SECTION_ORDER;
+
   return (
     <>
       <Head>
         <title>About Us - NULP | National Urban Learning Platform</title>
-        <meta name="description" content="Learn about NULP's vision, mission, and approach to urban learning and capacity building for urban practitioners." />
+        <meta
+          name="description"
+          content="Learn about NULP's vision, mission, and approach to urban learning and capacity building for urban practitioners."
+        />
       </Head>
 
       {/* Header */}
@@ -24,25 +58,13 @@ const AboutPage: React.FC = () => {
       {/* About Us Hero Section */}
       <AboutUsHero />
 
-      {/* Vision Section */}
-      <AboutUsVision />
+      {/* Dynamically ordered sections */}
+      {sections.map((slug) => {
+        const Section = sectionComponents[slug];
+        return Section ? <Section key={slug} /> : null;
+      })}
 
-      {/* Mission Section */}
-      <AboutUsMission />
-
-      {/* Our Approach Section */}
-      <AboutUsApproach />
-
-      {/* Our Offerings Section */}
-      <AboutUsOfferings />
-
-      {/* Why Does NULP Exist Section */}
-      <AboutUsWhy />
-
-      {/* Partner with Us Section */}
-      <AboutUsPartner />
-
-      {/* Get in Touch Section */}
+      {/* Get in Touch Section (not part of ordering) */}
       <AboutUsContact />
 
       {/* Footer */}
@@ -51,4 +73,30 @@ const AboutPage: React.FC = () => {
   );
 };
 
-export default AboutPage; 
+export const getStaticProps: GetStaticProps<AboutPageProps> = async () => {
+  const allowedSlugs = new Set(DEFAULT_SECTION_ORDER);
+  const res = await articlesApi.getAboutUsArticles();
+  let orderedSectionSlugs = DEFAULT_SECTION_ORDER;
+
+  if (res.success && Array.isArray(res.data)) {
+    const items = (res.data as HomepageArticleItem[])
+      .filter((a) => allowedSlugs.has((a.slug || "").toLowerCase()))
+      .map((a) => ({
+        slug: (a.slug || "").toLowerCase(),
+        order: a.display_order ?? 0,
+      }))
+      .sort((a, b) => a.order - b.order)
+      .map((a) => a.slug);
+
+    if (items.length > 0) {
+      orderedSectionSlugs = items;
+    }
+  }
+
+  return {
+    props: { orderedSectionSlugs },
+    revalidate: 300,
+  };
+};
+
+export default AboutPage;
